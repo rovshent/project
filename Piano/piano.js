531d7fbcbb1e4b1ed99bde7918af29943b4ee10b
rovshen04 <tagangylyjowrowsen28@gmail.com>
2021-02-14 04:34:29 -0800

    made responsive

--- Piano/piano.js
+++ Piano/piano.js
@@ -1,10 +1,8 @@
 var a=document.querySelectorAll('button');
-console.log(a);
 var s;
+console.log(a);
 a.forEach(addEventListener("click",function(e) {
-    var t = e.target.id;
-    console.log(t);
-    s = document.querySelector('#'+t);
+    s = document.querySelector('#'+e.target.id);
     s.play();
     if(s.currentTime>3)s.pause();
     s.currentTime=0;
